e758aaefe01eb8d471c710cf98bea8781da9b382
wanghaichao <wanghaichao@didachuxing.com>
2021-12-31 17:45:23 +0800

    listener 调试成功

--- demoPlugin/src/main/groovy/com.hc.demoplugin/PluginImpl.groovy
+++ demoPlugin/src/main/groovy/com.hc.demoplugin/PluginImpl.groovy
@@ -10,5 +10,14 @@ public class PluginImpl implements Plugin<Project>{
         System.out.println("========================");
         System.out.println("hello my first plugin......!");
         System.out.println("========================");
+
+        //addListener 与 registerTransfrom 好像是不能同时使用
+        project.gradle.addListener(new TaskListener())
+
+//        //使用自己的 transform
+//        def android = project.extensions.findByType(AppExtension);
+//        android.registerTransform(new InsertTransform())
+
+
     }
 }
